5cba25d1e94433907da2c14d1e2bad873cb8a132
diCharles <43318661+diCharles@users.noreply.github.com>
2019-03-25 13:23:59 -0600

    squeleton off library FlexTimer

---
+++ FlexTimer.c
@@ -0,0 +1,80 @@
+/*
+ * FlexTimer.c
+ *
+ *  Created on: 25/03/2019
+ *      Author: Charles
+ *     brief: implementation of funcions in FlexTimer.h
+ */
+#include "FlexTimer.h"
+
+void FlexTimer_update_channel_value(int16_t channel_value)
+{
+	/**Assigns a new value for the duty cycle*/
+	FTM0->CONTROLS[0].CnV = channel_value;
+}
+
+
+void FlexTimer_Init(flexTimer_channels_t channel)
+{
+
+	/**Clock gating for FlexTimer*/
+	SIM->SCGC6 |= SIM_SCGC6_FTM0(1);
+	Fleximer_mode(CHANNEL_0,TOGGLE_OUTPUT_ON_MATCH ,TOGGLE_OUTPUT_ON_MATCH);
+	FlexTimer_update_channel_value(0x03);
+}
+void Fleximer_mode(flexTimer_channels_t channel, flexTimer_modes_t mode, flexTimer_mode_configurations_t config)
+{
+
+	switch(mode)
+	{
+	case OUTPUT_COMPARE:
+		/**It enable the FTM*/
+		FTM0->MODE |= FTM_MODE_FTMEN_MASK;
+		/**Selects the FTM behavior in BDM mode.In this case in functional mode*/
+		FTM0->CONF |= FTM_CONF_BDMMODE(3);
+		/**Assign modulo register with a predefined value*/
+		FTM0->MOD = 0x05;
+
+		if( TOGGLE_OUTPUT_ON_MATCH== config)
+			/**Configure FlexTimer in output compare in toggle mode*/
+			FTM0->CONTROLS[0].CnSC = FTM_CnSC_MSA(1) | FTM_CnSC_ELSA(1);
+		else if(CLEAR_OUTPUT_ON_MATCH== config)
+			/**Configure FlexTimer in output compare in clear output on match*/
+			FTM0->CONTROLS[0].CnSC = FTM_CnSC_MSA(1) | FTM_CnSC_ELSB(1);
+		else if(SET_OUTPUT_ON_MATCH== config)
+			/**Configure FlexTimer in output compare  in set output on match*/
+					FTM0->CONTROLS[0].CnSC = FTM_CnSC_MSA(1) | FTM_CnSC_ELSB(1) | FTM_CnSC_ELSA(1);
+		/**Select clock source and prescaler*/
+		FTM0->SC |= FTM_SC_CLKS (FLEX_TIMER_CLKS_1)| FTM_SC_PS(FLEX_TIMER_PS_128);
+
+		break;
+	case TIMER_OVERFLOW:
+		FTM0->MODE |= FTM_MODE_FTMEN_MASK;
+		/**Selects the FTM behavior in BDM mode.In this case in functional mode*/
+		FTM0->CONF |= FTM_CONF_BDMMODE(3);
+		/**Assigning a default value for modulo register*/
+		FTM0->MOD = 0xF0;
+		/**Enabling the interrupt,selecting the clock source and a pre-scaler of 128*/
+		FTM0->SC |=  FTM_SC_TOIE(1) |FTM_SC_CLKS (FLEX_TIMER_CLKS_1)| FTM_SC_PS(FLEX_TIMER_PS_128);
+		break;
+	case PWM		   :
+		/**When write protection is enabled (WPDIS = 0), write protected bits cannot be written.
+		 * When write protection is disabled (WPDIS = 1), write protected bits can be written.*/
+		FTM0->MODE |= FTM_MODE_WPDIS_MASK;
+		/**Enables the writing over all registers*/
+		FTM0->MODE &= ~ FTM_MODE_FTMEN_MASK;
+		/**Assigning a default value for modulo register*/
+		FTM0->MOD = 0x00FF;
+		/**Selects the Edge-Aligned PWM mode mode*/
+		FTM0->CONTROLS[0].CnSC = FTM_CnSC_MSB(1) | FTM_CnSC_ELSB(1);
+		/**Assign a duty cycle of 50%*/
+		FTM0->CONTROLS[0].CnV = FTM0->MOD/2;
+		/**Configure the times*/
+		FTM0->SC |= FTM_SC_CLKS(FLEX_TIMER_CLKS_1)| FTM_SC_PS(FLEX_TIMER_PS_128);
+		break;
+
+	}
+
+}
+
+
---
+++ FlexTimer.h
@@ -0,0 +1,70 @@
+/*
+ * FlexTimer.h
+ *
+ *  Created on: 25/03/2019
+ *      Author: Charles
+ *      \brief
+		This is the header file for the FlexTimer divice driver.
+		Add configuration structures.
+ */
+
+
+#ifndef FLEXTIMER_H_
+#define FLEXTIMER_H_
+
+
+#include "MK64F12.h"
+
+#define FLEX_TIMER_CLKS_0  (0U)
+#define FLEX_TIMER_CLKS_1  (1U)
+#define FLEX_TIMER_CLKS_2  (2U)
+#define FLEX_TIMER_CLKS_3  (3U)
+
+
+#define FLEX_TIMER_PS_1    (0U)
+#define FLEX_TIMER_PS_2    (1U)
+#define FLEX_TIMER_PS_4    (2U)
+#define FLEX_TIMER_PS_8    (3U)
+#define FLEX_TIMER_PS_16    (4U)
+#define FLEX_TIMER_PS_32    (5U)
+#define FLEX_TIMER_PS_64    (6U)
+#define FLEX_TIMER_PS_128    (7U)
+
+
+#define  FLEX_TIMER_DMA   0x01
+#define  FLEX_TIMER_ELSA  0x04
+#define  FLEX_TIMER_ELSB  0x08
+#define  FLEX_TIMER_MSA   0x10
+#define  FLEX_TIMER_MSB   0x20
+#define  FLEX_TIMER_CHIE  0x40
+#define  FLEX_TIMER_CHF   0x80
+
+/*FLEX TIMER OPERATION MODES*/
+typedef enum{
+			OUTPUT_COMPARE,
+			TIMER_OVERFLOW,
+			PWM
+			}flexTimer_modes_t;
+/* FLEX TIMER CONFIGURATION OF OPERATION MODES*/
+typedef enum{
+			TOGGLE_OUTPUT_ON_MATCH,
+			CLEAR_OUTPUT_ON_MATCH,
+			SET_OUTPUT_ON_MATCH
+			}flexTimer_mode_configurations_t;
+/*FLEX TIMER CHANNELS*/
+typedef enum{CHANNEL_0,
+			CHANNEL_1,
+			CHANNEL_2,
+			CHANNEL_3
+			}flexTimer_channels_t;
+
+
+void FlexTimer_update_channel_value(int16_t channel_value);
+
+/*brief for flex timer initialization in a specific mode from flexTimer_mode_t*/
+/*channel argument still not implemented*/
+void FlexTimer_Init(flexTimer_channels_t channel);
+void Fleximer_mode(flexTimer_channels_t channel, flexTimer_modes_t mode, flexTimer_mode_configurations_t);
+
+
+#endif /* FLEXTIMER_H_ */
